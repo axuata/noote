afe3285678624c52b7721883e94c79fb7d643df8
axuata <axuata@gmail.com>
2025-07-08 18:01:58 +0900

    ✏️ Added GitHub repository link to the footer

--- components/layout/LFooter.vue
+++ components/layout/LFooter.vue
@@ -4,10 +4,10 @@
 
 <template>
   <footer class="text-center py-24px font-500 text-gray-8 text-shadow-sm">
-    <div class="flex justify-center py-4px">
-      <button>
+    <div class="flex justify-center my-4px">
+      <a href="https://github.com/axuata/noote">
         <Icon name="logos:github-icon" class="size-20px flex items-center justify-center" />
-      </button>
+      </a>
     </div>
     <p>© 2025 Axuata. Licensed under <a href="https://opensource.org/license/mit">the MIT License</a>.</p>
     <p>Icons by <a href="https://tabler.io/icons">Tabler Icons</a> and <a href="https://github.com/microsoft/fluentui-emoji">Fluent Emoji</a>.</p>
